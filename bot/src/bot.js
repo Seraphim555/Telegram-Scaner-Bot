78c0437d101c2e2c7c6fa9d02545fe7b08c652ae
Ryan Gosling <vjlev671games@gmail.com>
2025-12-01 02:55:30 +0500

    Добавил автоматическую регистрацию пользователя как массив в памяти

--- bot/src/bot.js
+++ bot/src/bot.js
@@ -1,5 +1,7 @@
 import { Telegraf } from "telegraf";
 import dotenv from "dotenv";
+import { users } from "./db.js";
+
 dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -7,22 +9,33 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(async (ctx) => {
     const user = ctx.from;
 
-    await ctx.reply(`Привет, ${user.first_name}! 👋
+    if (!users.has(user.id)) {
+        users.set(user.id, {
+            telegramId: user.id,
+            name: user.first_name ?? "Без имени"
+        });
+    }
+
+    await ctx.reply(
+        `Привет, ${user.first_name}! 👋
 Я бот для учета ключей.
-Нажми кнопку ниже, чтобы открыть сканер 👇`, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { 
-                        text: "Открыть сканер",
-                        web_app: {
-                            url: process.env.WEBAPP_URL
+
+Нажми кнопку ниже, чтобы открыть сканер 👇`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: "Открыть сканер",
+                            web_app: {
+                                url: process.env.WEBAPP_URL
+                            }
                         }
-                    }
+                    ]
                 ]
-            ]
+            }
         }
-    });
+    );
 });
 
 bot.launch();
---
+++ bot/src/db.js
@@ -0,0 +1,4 @@
+export const users = new Map();
+
+// userId = Telegram ID
+// users.set(userId, { name, telegramId })
